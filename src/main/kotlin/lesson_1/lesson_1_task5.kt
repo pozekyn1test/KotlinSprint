bb6a1381cd51dfeae2bee90e427fb079dcefcd78
Nikita Zekynov <nponomarenko13@mail.ru>
2024-01-24 15:36:05 +0300

    KS-1-5 ввел две константы. Привел время к нужному формату, используя String.format

--- src/main/kotlin/lesson_1/lesson_1_task5.kt
+++ src/main/kotlin/lesson_1/lesson_1_task5.kt
@@ -1,11 +1,14 @@
 package org.example.lesson_1
 
 fun main() {
+    val SECONDS_IN_MINUTE = 60
+    val MINUTES_IN_HOUR = 60
     val seconds: Short = 6480
-    val minutes = seconds / 60
-    val secondsLeft = seconds % 60
-    val hours = minutes / 60
-    val minutesLeft = minutes % 60
+    val minutes = seconds / SECONDS_IN_MINUTE
+    val secondsLeft = seconds % SECONDS_IN_MINUTE
+    val hours = minutes / MINUTES_IN_HOUR
+    val minutesLeft = minutes % MINUTES_IN_HOUR
 
-    println("01:48:00")
+
+    println(String.format("%02d:%02d:%02d", hours, minutesLeft, secondsLeft))
 }
