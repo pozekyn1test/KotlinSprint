ce6515cce661c1228b0cb0fc2449a775a385d2f1
Nikita Zekynov <nponomarenko13@mail.ru>
2024-01-24 23:42:21 +0300

    KS-2-4 Решение задачи

---
+++ src/main/kotlin/lesson_2/lesson_2_task4.kt
@@ -0,0 +1,11 @@
+package org.example.lesson_2
+
+fun main() {
+    val cristalOre = 7
+    val ironOre = 11
+
+    val buffMultiplier = 0.2
+
+    println((cristalOre * buffMultiplier).toInt())
+    println((ironOre * buffMultiplier).toInt())
+}
