d080e72e32696960a8f0e6373b2c8d1ae1c2cc03
Nikita Zekynov <nponomarenko13@mail.ru>
2024-01-25 21:27:05 +0300

    KS-3-1 Решение задания

---
+++ src/main/kotlin/lesson_3/lesson_3_task1.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_3
+
+fun main() {
+    var userName = "Никита"
+    var greeting: String
+
+    greeting = "Добрый день, $userName!"
+    println(greeting)
+
+    greeting = "Добрый вечер, $userName!"
+    println(greeting)
+
+}
