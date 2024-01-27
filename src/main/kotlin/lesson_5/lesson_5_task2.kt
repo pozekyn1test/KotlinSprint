c28729fc270e51e6d26bcef4fe333aaeb88b5372
Nikita Zekynov <nponomarenko13@mail.ru>
2024-01-27 12:21:10 +0300

    KS-5-2 Решение задачи

---
+++ src/main/kotlin/lesson_5/lesson_5_task2.kt
@@ -0,0 +1,13 @@
+package org.example.lesson_5
+
+const val AGE_OF_MAJORITY = 18
+fun main() {
+    val currentYear = 2024
+    println("Введите свой год рождения:")
+    val birthYear = readln().toInt()
+    val userAge = currentYear - birthYear
+
+    when {
+        userAge >= AGE_OF_MAJORITY -> println("Показать экран со скрытым контентом")
+    }
+}
