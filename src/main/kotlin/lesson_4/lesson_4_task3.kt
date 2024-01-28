381b3d1d568c5d26941a508603a736615d9e3799
Nikita Zekynov <nponomarenko13@mail.ru>
2024-01-28 12:59:42 +0300

    KS-4-3 Вынес солнечную погоду и открытый тент в константы

--- src/main/kotlin/lesson_4/lesson_4_task3.kt
+++ src/main/kotlin/lesson_4/lesson_4_task3.kt
@@ -2,12 +2,12 @@ package org.example.lesson_4
 
 const val GOOD_AIR_HUMIDITY = 20
 const val WINTER_SEASON = "Winter"
+const val SUNNY = true
+const val TENT_IS_OPENED = true
 fun main() {
-    val isSunny = true
-    val isOpened = true
     val airHumidity = 20
     val season = "Winter"
 
     println("Благоприятные ли условия сейчас для роста бобовых? " +
-            "${isSunny && isOpened && (airHumidity == GOOD_AIR_HUMIDITY) && (season != WINTER_SEASON)}")
+            "${SUNNY && TENT_IS_OPENED && (airHumidity == GOOD_AIR_HUMIDITY) && (season != WINTER_SEASON)}")
 }
