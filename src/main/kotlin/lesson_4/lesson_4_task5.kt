bad8cd85a78e1c9a49a958ba7939fd8f4e7fb5e1
Nikita Zekynov <nponomarenko13@mail.ru>
2024-01-29 19:52:43 +0300

    KS-4-5 Вынес значения в константы, убрал условие про поврежденный корабль, изменил минимальное число экипажа на верное

--- src/main/kotlin/lesson_4/lesson_4_task5.kt
+++ src/main/kotlin/lesson_4/lesson_4_task5.kt
@@ -1,5 +1,8 @@
 package org.example.lesson_4
 
+const val CREW_MIN = 55
+const val CREW_MAX = 70
+const val MIN_SUPPLIES = 50
 fun main() {
     println("Корабль поврежден?(true/false):")
     val isDamaged = readln().toBoolean()
@@ -14,8 +17,8 @@ fun main() {
     val isGoodWeather = readln().toBoolean()
 
     when {
-        (!isDamaged) && (crew in 50..70) && (cratesOfSupplies > 50) -> println("Корабль может приступить к плаванию")
-        (isDamaged) && (crew == 70) && (cratesOfSupplies >= 50) && (isGoodWeather) -> println("Корабль может приступить к плаванию")
+        (!isDamaged) && (crew in CREW_MIN..CREW_MAX) && (cratesOfSupplies > MIN_SUPPLIES) -> println("Корабль может приступить к плаванию")
+        (crew == CREW_MAX) && (cratesOfSupplies >= MIN_SUPPLIES) && (isGoodWeather) -> println("Корабль может приступить к плаванию")
         else -> println("Корабль не может приступить к плаванию")
     }
 }
